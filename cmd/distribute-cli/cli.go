2a015675bb087dadf795629eb9444e877ced75cc
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-06-19 20:53:19 +0200

    feat(io): Added functions to list a directory and list all pdfs.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- cmd/distribute-cli/cli.go
+++ cmd/distribute-cli/cli.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/ungrichtepfl/go-distribute/pkg/excel"
+	"github.com/ungrichtepfl/go-distribute/pkg/distribute"
 )
 
 func main() {
 	last_name_col := uint64(0)
-	raw_data, err := excel.EmailToName("./test/data/test_info.xlsx", 1, 3, &last_name_col, nil, nil, nil)
+	file_path := `test/data/test_info.xlsx`
+	println(file_path)
+	raw_data, err := distribute.EmailToName(file_path, 1, 3, &last_name_col, nil, nil, nil)
 	fmt.Println(raw_data)
+	fmt.Println(err)
+
+	dir := "test/data/documents"
+	files, err := distribute.ListPDFs(dir, false)
+	fmt.Println(files)
+	fmt.Println(err)
+	files, err = distribute.ListPDFs(dir, true)
+	fmt.Println(files)
 	fmt.Println(err)
 
 }
---
+++ pkg/distribute/io.go
@@ -0,0 +1,67 @@
+package distribute
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func ListPDFs(path string, recursive bool) ([]string, error) {
+	all_files, err := listDirectory(path, recursive)
+	if err != nil {
+		return nil, err
+	}
+
+	pdfs := make([]string, 0, len(all_files))
+	for _, file := range all_files {
+		if filepath.Ext(file) == ".pdf" {
+			pdfs = append(pdfs, file)
+		}
+	}
+	return pdfs, nil
+}
+
+func listDirectory(path string, recursive bool) ([]string, error) {
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if !info.IsDir() {
+		return nil, fmt.Errorf("path %s is not a directory", path)
+	}
+
+	sep_count := strings.Count(path, string(filepath.Separator))
+	depth := func(path string) int {
+		return strings.Count(path, string(filepath.Separator)) - sep_count
+	}
+
+	var files []string
+	const max_depth = 0
+	err = filepath.WalkDir(path, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			if !recursive && depth(path) > max_depth {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no files found in %s", path)
+	}
+
+	return files, nil
+
+}
